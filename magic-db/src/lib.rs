3146ad38f24d7aca494c23557b6452f3c7518006
qjerome <qjerome@rawsec.lu>
2025-11-13 14:26:21 +0100

    add: test for magic-db

--- magic-db/src/lib.rs
+++ magic-db/src/lib.rs
@@ -2,3 +2,18 @@ use magic_embed::magic_embed;
 
 #[magic_embed(include=["magic-db/src/magdir"], exclude=["magic-db/src/magdir/der"])]
 pub struct CompiledDb;
+
+#[cfg(test)]
+mod test {
+    use crate::CompiledDb;
+    use std::{env, fs::File};
+
+    #[test]
+    fn test_compiled_db() {
+        let db = CompiledDb::open().unwrap();
+        let mut exe = File::open(env::current_exe().unwrap()).unwrap();
+        let magic = db.magic_first(&mut exe, None).unwrap();
+        println!("{}", magic.message());
+        assert!(!magic.is_default())
+    }
+}
